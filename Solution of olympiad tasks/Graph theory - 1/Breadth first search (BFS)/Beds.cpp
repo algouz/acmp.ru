9bd0547e9261aa873a1dfb47337df01cd2a8eb5a
Shohruh <rshohrux16@gmail.com>
2023-02-03 11:42:08 +0500

    shorturl.at/fqwY2

--- Solution of olympiad tasks/Graph theory - 1/Breadth first search (BFS)/Beds.cpp
+++ Solution of olympiad tasks/Graph theory - 1/Breadth first search (BFS)/Beds.cpp
@@ -5,17 +5,17 @@ using namespace std;
 int main(){
     int n, m, cnt=0;
     cin >> n >> m;
-    int dx[] = {0, 0, 1, -1};
-    int dy[] = {-1, 1, 0, 0};
+    int dx[] = {1, 0, 0, -1};
+    int dy[] = {0, -1, 1, 0};
     queue<pair<int, int> > b;
     vector<string> g(n);
     for(int i=0; i<n; i++)
-        for(int j=0; j<m; j++)
-            cin >> g[i][j];
+        cin >> g[i];
     for(int i=0; i<n; i++)
         for(int j=0; j<m; j++){
             if(g[i][j] == '#'){
                 cnt ++;
+                g[i][j] = '@';
                 b.push({i, j});
                 while(!b.empty()){
                     int x = b.front().first, y = b.front().second;
@@ -23,9 +23,8 @@ int main(){
                     for(int u=0, X, Y; u<4; u++){
                         X = x + dx[u];
                         Y = y + dy[u];
-                        if(X < 0 || X >= n || Y < 0 || Y >= m) continue;
-                        if(g[X][Y] != '#') continue;
-                        g[X][Y] = '.';
+                        if(X < 0 || X >= n || Y < 0 || Y >= m || g[X][Y] != '#') continue;
+                        g[X][Y] = '@';
                         b.push({X, Y});
                     }
                 }
